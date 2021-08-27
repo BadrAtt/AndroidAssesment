899177277d9e2f1b2f1a5f69b2e406227c69842c
BadrElattaoui <badr.e@wib.me>
2021-08-28 00:37:39 +0100

    add networking setup

---
+++ app/src/main/java/com/elattaoui/moviedb/di/NetworkingModule.kt
@@ -0,0 +1,55 @@
+package com.elattaoui.moviedb.di
+
+import com.elattaoui.moviedb.networking.Api
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.moczul.ok2curl.CurlInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import timber.log.Timber
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkingModule {
+
+    @Singleton
+    @Provides
+    internal fun providesOkHttp(): OkHttpClient {
+
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .addInterceptor(CurlInterceptor {
+                Timber.d("Ok2Curl $it ")
+            })
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    }
+
+
+    @Singleton
+    @Provides
+    fun providesApi(httpClient: OkHttpClient): Api {
+        val contentType = "application/json".toMediaType()
+        val json = Json {
+            isLenient = true
+            ignoreUnknownKeys = true
+        }
+
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .client(httpClient)
+            .build()
+            .create(Api::class.java)
+    }
+}
---
+++ app/src/main/java/com/elattaoui/moviedb/networking/Api.kt
@@ -0,0 +1,3 @@
+package com.elattaoui.moviedb.networking
+
+interface Api {}
